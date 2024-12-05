99c8e91bee5b6be12817c73ef174531a5155c44b
Antonio <98595473+tonujet@users.noreply.github.com>
2024-12-05 15:59:01 +0200

    c3_p1 completed

--- src/c3_consensus/p1_pow.rs
+++ src/c3_consensus/p1_pow.rs
@@ -4,6 +4,7 @@
 //! This is the same logic we implemented previously. Here we re-implement it in the
 //! generic consensus framework that we will use throughout the rest of the chapter.
 
+use crate::hash;
 use super::{Consensus, Header};
 
 /// A Proof of Work consensus engine. This is the same consensus logic that we
@@ -19,24 +20,41 @@ impl Consensus for Pow {
     /// Check that the provided header's hash is below the required threshold.
     /// This does not rely on the parent digest at all.
     fn validate(&self, _: &Self::Digest, header: &Header<Self::Digest>) -> bool {
-        todo!("Exercise 1")
+        hash(header) < self.threshold
     }
 
     /// Mine a new PoW seal for the partial header provided.
     /// This does not rely on the parent digest at all.
     fn seal(&self, _: &Self::Digest, partial_header: Header<()>) -> Option<Header<Self::Digest>> {
-        todo!("Exercise 2")
+        let mut h = Header {
+            parent: partial_header.parent,
+            height: partial_header.height,
+            state_root: partial_header.state_root,
+            extrinsics_root: partial_header.extrinsics_root,
+            consensus_digest: 0,
+        };
+        
+        while !self.validate(&0, &h) {
+            h.consensus_digest = h.consensus_digest + 1;            
+        }
+        
+        Some(h)
     }
 }
 
 /// Create a PoW consensus engine that has a difficulty threshold such that roughly 1 in 100 blocks
 /// with randomly drawn nonces will be valid. That is: the threshold should be u64::max_value() / 100.
 pub fn moderate_difficulty_pow() -> Pow {
-    todo!("Exercise 3")
+    Pow {
+        threshold: u64::MAX / 100,
+    }
 }
 
 /// Create an instance of the PoW Consensus that behaves identically to the trivial
 /// consensus implementation for `()` from the module level.
 pub fn trivial_always_valid_pow() -> Pow {
-    todo!("Exercise 4")
+    Pow {
+        threshold: u64::MAX,
+    }
+    
 }
